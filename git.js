4a7affb8fb3255c970fe3bd363f4a5c9fe77a4ab
Adil4961 <adilofficial.4961@gmail.com>
2023-02-06 14:58:27 +0530

    add new files

---
+++ git.js
@@ -0,0 +1,16 @@
+let a = Number(prompt("what is your Birth year"))
+let currentYear = 2023
+let b= currentYear-a
+if(b){
+    prompt(b+ " years old?")}
+    else if(b==="yes"){
+     
+    }
+
+{
+    if(b>=18){
+        alert("congratulations your are able to vote")
+    }else if (b<18){
+        alert("you are minor now")
+    }
+}
